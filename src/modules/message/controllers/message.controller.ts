d84584b4441ecbf97605a2d9564b115baa89fba1
hoangdatit <ddhoangdat.it@gmail.com>
2023-06-14 13:53:59 +0700

    fix: api version tag

--- src/modules/message/controllers/message.controller.ts
+++ src/modules/message/controllers/message.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post, Query, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query, UseGuards, Version } from '@nestjs/common';
 import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 
 import { PaginationRequestDto } from '../../../common/dto/pagination-request.dto';
@@ -16,12 +16,14 @@ export class MessageController {
     constructor(private readonly _messageService: MessageService) {}
 
     @ApiOperation({ summary: 'Get messages from family chat' })
+    @Version('1')
     @Get('/messages')
     async getMessages(@Query() params: PaginationRequestDto): Promise<PaginationResponseDto<MessageEntity>> {
         return await this._messageService.getMessages(params);
     }
 
     @ApiOperation({ summary: 'Create message to family chat' })
+    @Version('1')
     @Post()
     async createNewMessage(@Body() request: CreateMessageDto): Promise<MessageEntity> {
         return await this._messageService.createMessage(request);
